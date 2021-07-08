2cf4892e0da2044565138689cc832532e8e60748
bekzemed <bereketzemed@gmail.com>
2021-07-08 14:04:22 +0300

    remove file from gitignore file

---
+++ src/config/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseUtil = {
+  apiKey: 'AIzaSyAe31itiQ9ETNOCuVOcSxJGkmfobnA_zJg',
+  authDomain: 'dexter-50d8c.firebaseapp.com',
+  projectId: 'dexter-50d8c',
+  storageBucket: 'dexter-50d8c.appspot.com',
+  messagingSenderId: '172479042581',
+  appId: '1:172479042581:web:e881b235ee16de8870ea1c',
+  measurementId: 'G-H95KNZQTN2',
+};
